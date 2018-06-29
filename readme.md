c3be7cb0c6f63c8c3e71ec444476241c2573cc0a
jared <jaredspecjr@gmail.com>
2018-06-29 13:06:52 -0700

    trying to fix inside of list boxes not dispaying info correctly

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-Notes: setting up an environment is hard! ran into several errors trying to copy paste and remove certain components. this did not work. I'm starting the environment from scratch.
+Notes:
+
+setting up an environment is hard! ran into several errors trying to copy paste and remove certain components. this did not work. I'm starting the environment from scratch.
+
+I'm noticing a that the same css i used for angular will not work for react.
+I have to tweak it a little to get it to look the same.
--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,12 +4,16 @@ import PropTypes from "prop-types";
 function Keg(props){
   return (
     <div className="listStyle">
-      <div>
+      <div className="infoStyle">
         {props.name} --- {props.type}<br></br>{props.price} Pints: {props.pints}<br></br> ABV: {props.abv}
       </div>
-      <button className="button">Sell a Pint!</button>
+      <div className="button">
+        <button className="buttonStyle">Sell a Pint!</button>
+      </div>
         <style jsx>{`
           .listStyle {
+            display: flex;
+            flex-direction: row;
             width: 270px;
             font-size: 25px;
             font-family: comic;
@@ -28,7 +32,17 @@ function Keg(props){
           .listStyle:nth-child(3){
             background-color: #17a2b8;
           }
-      
+          .button{
+          align-self: flex-end;
+          height: 30px;
+          }
+          .buttonStyle{
+            width: 100px;
+          }
+          .infoStyle{
+            width:800px;
+          }
+
       `}</style>
     </div>
   );
